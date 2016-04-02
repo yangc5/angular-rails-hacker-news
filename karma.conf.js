397a0853e57350ebc55ee58cdd04609388925700
Chen Yang <yangc5@mail.uc.edu>
2016-04-02 12:52:13 -0500

    generated and configured karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+// Karma configuration
+// Generated on Sat Apr 02 2016 12:46:56 GMT-0500 (CDT)
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'js/angular.js',
+      'js/angular-mocks.js',
+      'js/app/**/*.js',
+      'tests/*.spec.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+    },
+    reporters: ['spec'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Chrome'],
+    singleRun: true,
+    concurrency: Infinity
+  })
+}
