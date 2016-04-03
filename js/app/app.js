36c0f17aa2df24a08da2228a9fe9f7f5283f711e
Chen Yang <yangc5@mail.uc.edu>
2016-04-03 10:46:32 -0500

    refactored posts view

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,11 @@
-angular.module('flapperNews', []);
+angular.module('flapperNews', ['ui.router'])
+       .config(function($stateProvider, $urlRouterProvider) {
+                $stateProvider
+                  .state('posts', {
+                    url: '/posts',
+                    templateUrl: 'views/posts.html',
+                    controller: 'MainController as ctrl'
+                  });
+
+                $urlRouterProvider.otherwise('/');
+              });
